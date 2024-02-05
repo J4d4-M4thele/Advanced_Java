b1f66635da75844206f44644a8dbd17b26bdd4f9
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2024-02-05 18:44:16 +0200

    first commit

---
+++ src/ex04_3_exercise/Main.java
@@ -0,0 +1,18 @@
+package ex04_3_exercise;
+
+public class Main {
+  public static void main(String[] args) {
+    PeerSingleton peer01 = PeerSingleton.getInstance();
+    PeerSingleton peer02 = PeerSingleton.getInstance();
+
+    for(String hostName: peer01.getHostNames()) {
+      System.out.println("Host Name: " + hostName);
+    }
+
+    System.out.println();
+
+    for(String hostName: peer02.getHostNames()) {
+      System.out.println("Host Name: " + hostName);
+    }
+  }  
+}
