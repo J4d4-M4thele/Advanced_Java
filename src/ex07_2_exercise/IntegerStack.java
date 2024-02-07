c2f94b4e38c5bd8b248cdcdf6d0405c035ec664c
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2024-02-07 12:33:23 +0200

    first commit

---
+++ src/ex07_2_exercise/IntegerStack.java
@@ -0,0 +1,11 @@
+package ex07_2_exercise;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class IntegerStack {
+
+    private Deque<Integer> data = new ArrayDeque<Integer>();
+
+    
+}
