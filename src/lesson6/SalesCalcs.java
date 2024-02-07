f89006688c93bf868de62ea07648a53caaca45eb
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2024-02-07 10:08:14 +0200

    first commit

--- src/lesson6/SalesCalcs.java
+++ src/lesson6/SalesCalcs.java
@@ -1,28 +1,27 @@
 package lesson6;
 
 public interface SalesCalcs {
-
   public static final String reportTitle="\n==Static List Report==";
+  
   public String getName();
   public double calcSalesPrice();
   public double calcCost();
   public double calcProfit();
-
-  //using default keyword to add body to method
-  public default void printItemData(SalesCalcs item) {
-    System.out.println("--" + item.getName() + " Report--");
-    System.out.println("Sales Price: " + item.calcSalesPrice());
-    System.out.println("Cost: " + item.calcCost());
-    System.out.println("Profit: " + item.calcProfit());
-  }
-
+  
   public static void printItemArray(SalesCalcs[] items){
     System.out.println(reportTitle);
-    for(SalesCalcs item:items){
-      System.out.println("--" + item.getName() + " Report--");
-      System.out.println("Sales Price: " + item.calcSalesPrice());
-      System.out.println("Cost: " + item.calcCost());
-      System.out.println("Profit: " + item.calcProfit());
-    }
+      for(SalesCalcs item:items){
+        System.out.println("--" + item.getName() + " Report--");
+        System.out.println("Sales Price: " + item.calcSalesPrice());
+        System.out.println("Cost: " + item.calcCost());
+        System.out.println("Profit: " + item.calcProfit());
+      }
+  }
+  
+  public default void printItemReport(){
+    System.out.println("--" + this.getName() + " Report--");
+    System.out.println("Sales Price: " + this.calcSalesPrice());
+    System.out.println("Cost: " + this.calcCost());
+    System.out.println("Profit: " + this.calcProfit());    
   }
 }
