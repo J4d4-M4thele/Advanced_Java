9d813c0a4902a6b338575bf9437ed121332b35e7
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2024-02-09 12:38:58 +0200

    update

---
+++ src/ex10_8_exercise/GroupTest.java
@@ -0,0 +1,21 @@
+package ex10_8_exercise;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author oracle
+ */
+public class GroupTest {
+    
+    public static void main(String[] args) {
+
+        List<SalesTxn> tList = SalesTxn.createTxnList();
+        Map<String, List<SalesTxn>> tMap;
+        
+        // Print out transactions grouped by Buyer
+        System.out.println("=== Transactions Grouped by Buyer ===");
+        
+    }
+}
---
+++ src/lesson11/TestMultiCatch.java
@@ -0,0 +1,20 @@
+package lesson11;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class TestMultiCatch {
+    public static void main(String[] args) {
+//        try{
+//            System.out.println("About to open a file");
+//            //allows you to open files
+//            InputStream in = new FileInputStream("missingfile.txt");
+//            System.out.println("File open");
+//            int data = in.read();
+//            in.close();
+//        }catch(FileNotFoundException e){
+//
+//        }
+    }
+}
