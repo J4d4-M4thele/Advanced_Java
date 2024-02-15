f782e99a7c81bc650471e744fc48d75c910a4d23
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2024-02-15 08:32:40 +0200

    first commit

---
+++ src/ex15_1_exercise/CountRunnable.java
@@ -0,0 +1,13 @@
+package ex15_1_exercise;
+
+public class CountRunnable implements Runnable{
+  
+   //final int count;
+  //final String threadName;
+  
+  // Setup Constructor to initialize variables
+  
+  public void run(){ 
+    // Count with for loop
+  }
+}
---
+++ src/ex15_1_exercise/Main.java
@@ -0,0 +1,13 @@
+package ex15_1_exercise;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // Setup Executor
+    
+    // Submit Runnable
+    
+    // Showndown ExectuorService
+
+  }
+}
